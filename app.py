08d8b4e46f28455987b2c9fd9ba06d9522288d5d
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-06-01 23:24:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import pip
-pip.main(["install", "openpyxl"])
+#import pip
+#pip.main(["install", "openpyxl"])
 from openpyxl import reader,load_workbook,Workbook
 import numpy as np
 from PIL import Image
